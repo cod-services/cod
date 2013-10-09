eb457c90079970277a42578bfdd8251add67afaf
Sam Dodrill <shadow.h511@gmail.com>
2013-10-09 08:33:18 -0700

    Spit error on unreadable configuration file

--- src/config.py
+++ src/config.py
@@ -1,10 +1,16 @@
 import json
+import sys
 
 class Config():
     def __init__(self, cfilepath):
         self.cfilepath = cfilepath
 
-        with open(cfilepath, "r") as cfile:
-            configstr = cfile.read()
+        try:
+            with open(cfilepath, "r") as cfile:
+                configstr = cfile.read()
 
-            self.config = json.loads(configstr)
+                self.config = json.loads(configstr)
+        except IOError as e:
+            print "Config file not found or readable %s" % cfilepath
+            sys.exit(-1)
+
