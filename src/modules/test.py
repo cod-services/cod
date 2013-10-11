285e7e3eb743b8aeadd17355c358411cc519d727
Sam Dodrill <shadow.h511@gmail.com>
2013-10-11 13:07:43 -0700

    refactor test module

--- src/modules/test.py
+++ src/modules/test.py
@@ -2,11 +2,11 @@ from structures import *
 from utils import *
 
 def initModule(cod):
-    cod.botcommands["TEST"] = [commandTEST]
+    cod.botcommands["TEST"] = [test]
 
 def destroyModule(cod):
     del cod.botcommands["TEST"]
 
-def commandTEST(cod, line, splitline, source, destination):
+def test(cod, line, splitline, source, destination):
     reply(cod, source, destination, "Hello!")
 
