f828733944c4c200361e01df1c8ed59fad09f941
Sam Dodrill <shadow.h511@gmail.com>
2013-10-10 21:07:34 -0700

    Add configurability of NickServ identification

--- src/cod.py
+++ src/cod.py
@@ -85,7 +85,8 @@ class Cod():
         if self.config["etc"]["relayhostserv"]:
             commands["PRIVMSG"].append(relayHostServToOpers)
 
-        self.privmsg("NickServ", "ID ShadowNET %s" % self.config["me"]["nspass"])
+        self.privmsg("NickServ", "ID %s %s" % \
+                (self.config["me"]["acctname"], self.config["me"]["nspass"]))
 
         self.log("Cod initialized", "!!!")
 
