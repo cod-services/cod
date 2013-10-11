d4f73e5fa59984102d0174ff2120ee89a64dc81b
Sam Dodrill <shadow.h511@gmail.com>
2013-10-11 13:08:50 -0700

    Purge dead code

--- src/botcommands.py
+++ src/botcommands.py
@@ -1,19 +1,5 @@
 from structures import *
 import rblwatch
-import mpd
-from random import randint
-
-#Initialization for military operation name generator
-
-prefix = []
-suffix = []
-
-def commandTEST(cod, line, splitline, source, destination):
-    if destination == source:
-        #In a PM, the destination will anways be the source
-        cod.notice(destination, "Hello!")
-    else:
-        cod.privmsg(destination, "Hello!")
 
 def commandJOIN(cod, line, splitline, source, destination):
     if failIfNotOper(cod, cod.clients[source]):
@@ -71,29 +57,6 @@ def commandRBL(cod, line, splitline, source, destination):
 
     search.print_results()
 
-def commandMPD(cod, line, splitline, source, destination):
-    if not cod.config["mpd"]["enable"]:
-        return
-
-    if splitline[1].upper() == "FIND":
-        query = " ".join(splitline[2:])
-
-        cod.privmsg(destination, "Searching for %s" % query)
-
-        results = cod.mpd.find("any", query)
-
-        client = cod.clients[source]
-
-        for result in results:
-            cod.privmsg(destination, "%s: %s -- %s" %
-                    (client.nick, result["artist"], result["title"]))
-
-    elif splitline[1].upper() == "STATUS":
-        cur = cod.mpd.currentsong()
-
-        cod.privmsg(destination, "%s -- %s -- %4.2f%%" %
-                (cur["artist"], cur["title"], float(cur["pos"])/float(cur["time"])))
-
 def commandREHASH(cod, line, splitline, source, destination):
     if failIfNotOper(cod, cod.clients[source]):
         return
@@ -102,44 +65,6 @@ def commandREHASH(cod, line, splitline, source, destination):
 
     client = cod.clients[source]
     cod.servicesLog("REHASH: %s" % client.nick)
-
-def commandOPNAMEinit(cod, line, splitline, source, destination):
-    global prefix, suffix
-
-    if not prefix == []:
-        return
-
-    #Prepare lists
-    prefixfile = open(cod.config["etc"]["prefixfile"], 'r')
-    suffixfile = open(cod.config["etc"]["suffixfile"], 'r')
-    prefix = prefixfile.readlines()
-    suffix = suffixfile.readlines()
-    prefixfile.close()
-    suffixfile.close()
-
-    #Strip the lists
-    for junk in range(len(prefix)-1, -1, -1):
-        prefix[junk] = prefix[junk].strip()
-        if len(prefix[junk]) == 0:
-            prefix.pop(junk)
-    for junk in range(len(suffix)-1, -1, -1):
-        suffix[junk] = suffix[junk].strip()
-        if len(suffix[junk]) == 0:
-            suffix.pop(junk)
-
-def commandOPNAME(cod, line, splitline, source, destination):
-    #Format string
-    if (randint(0,9)==0):
-        phrase = "OPERATION %s %s %s" % \
-            (prefix[randint(0, len(prefix) - 1)],
-                prefix[randint(0, len(prefix) - 1)],
-                suffix[randint(0, len(suffix) - 1)]) #3
-    else:
-        phrase = "OPERATION %s %s" % \
-            (prefix[randint(0, len(prefix) - 1)],
-                suffix[randint(0, len(suffix) - 1)]) #2
-
-    cod.privmsg(destination, phrase.upper())
 
 def commandDIE(cod, line, splitline, source, destination):
     if failIfNotOper(cod, cod.clients[source]):
