f6f55f348e688f6ea38aba8cf0e186f062d5d1b5
Sam Dodrill <shadow.h511@gmail.com>
2013-10-11 21:02:18 -0700

    Fix NameErrors when unloading some modules

--- modules/mpdclient.py
+++ modules/mpdclient.py
@@ -4,11 +4,16 @@ from utils import *
 NAME="MPD Client"
 DESC="Does simple lookups and status on an mpd server"
 
+global mpd
+
 mpd = MPDClient()
 
 def initModule(cod):
+    global mpd
+
     cod.log("Establishing connection to MPD server", "===")
 
+    mpd = MPDClient()
     mpd.timeout = 10
     mpd.idletimeout = None
     mpd.connect(cod.config["mpd"]["host"], cod.config["mpd"]["port"])
@@ -18,14 +23,19 @@ def initModule(cod):
     cod.botcommands["MPD"] = [commandMPD]
 
 def destroyModule(cod):
+    global mpd
+
     cod.log("Disconnecting from MPD server", "===")
 
     mpd.close()
     mpd.disconnect()
 
     del cod.botcommands["MPD"]
+    del mpd
 
 def commandMPD(cod, line, splitline, source, destination):
+    global mpd
+
     if len(splitline) < 2:
         cod.reply(source, destination, "Not enough arguments")
         return
