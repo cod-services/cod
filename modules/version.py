0f02f8449e38d1d9df76da45c5bb00e6c369047d
Sam Dodrill <shadow.h511@gmail.com>
2013-10-11 17:06:26 -0700

    New module: version

---
+++ modules/version.py
@@ -0,0 +1,9 @@
+def initModule(cod):
+    cod.botcommands["VERSION"] = [commandVERSION]
+
+def destroyModule(cod):
+    del cod.botcommands["VERSION"]
+
+def commandVERSION(cod, line, splitline, source, destination):
+    cod.notice(source, "Cod version %s" % cod.version)
+
