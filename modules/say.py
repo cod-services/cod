927caafc0474bc5569b2ca40b6a3b01c0268f157
Sam Dodrill <shadow.h511@gmail.com>
2013-10-12 08:53:53 -0700

    SAY command module added

---
+++ modules/say.py
@@ -0,0 +1,20 @@
+NAME="Say command"
+DESC="Sends arbitrary text to a channel"
+
+from utils import *
+
+def initModule(cod):
+    cod.botcommands["SAY"] = [say]
+
+def destroyModule(cod):
+    del cod.botcommands["SAY"]
+
+def say(cod, line, splitline, source, destination):
+    if failIfNotOper(cod, cod.clients[source]):
+        return
+
+    cod.privmsg(splitline[1], " ".join(splitline[2:]))
+
+    cod.servicesLog("SAY: %s to %s" %
+            (cod.clients[source].nick, splitline[1]))
+
