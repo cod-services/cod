4a6d6f32f0b03c78f3a9782f9e316a90eed62570
Sam Dodrill <shadow.h511@gmail.com>
2013-10-13 20:02:04 -0700

    Do a bit more in the MPD client module

--- modules/mpdclient.py
+++ modules/mpdclient.py
@@ -13,7 +13,7 @@ def initModule(cod):
 
     cod.log("Establishing connection to MPD server", "===")
 
-    mpd = MPDClient()
+    mpd = MPDClient(use_unicode=True)
     mpd.timeout = 10
     mpd.idletimeout = None
     mpd.connect(cod.config["mpd"]["host"], cod.config["mpd"]["port"])
@@ -56,6 +56,42 @@ def commandMPD(cod, line, splitline, source, destination):
         mpd.update()
         cur = mpd.currentsong()
 
-        cod.reply(destination, source, "%s -- %s -- %4.2f%%" % \
+        cod.reply(source, destination, "%s -- %s -- %4.2f%%" % \
                 (cur["artist"], cur["title"], float(cur["pos"])/float(cur["time"])))
+
+    if failIfNotOper(cod, cod.client, cod.clients[source]):
+        return
+
+    if splitline[1].upper() == "NEXT":
+        mpd.next()
+
+        cod.reply(source, destination, "Next song is playing")
+
+    elif splitline[1].upper() == "PREV":
+        mpd.previous()
+
+        cod.privmsg(destination, "Previous song is playing")
+
+    elif splitline[1].upper() == "PAUSE":
+        mpd.pause()
+
+        cod.reply(source, destination, "Paused")
+
+    elif splitline[1].upper() == "PLAY":
+        if len(splitline) < 3:
+            mpd.play()
+            cod.reply(source, destination, "Playing")
+        else:
+            mpd.play(splitline[2])
+
+    elif splitline[1].upper() == "LISTSTATUS":
+        mpd.update()
+        mpd.iterate = True
+
+        cur = mpd.currentsong()
+
+        for line in mpd.playlistinfo():
+            cod.reply(source, destination, "%s%s: %s" % \
+                    (("* " if cur["id"] == line["id"] else "  "),
+                        line["artist"], line["title"]))
 
