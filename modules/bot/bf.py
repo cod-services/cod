a152c3b87041d57c60c39524577b8e52c67c3d70
Sam Dodrill <shadow.h511@gmail.com>
2013-11-06 15:05:15 -0800

    Add skybot's brainfuck interpreter

---
+++ modules/bot/bf.py
@@ -0,0 +1,101 @@
+'''brainfuck interpreter adapted from (public domain) code at
+http://brainfuck.sourceforge.net/brain.py'''
+
+import re
+
+BUFFER_SIZE = 5000
+MAX_STEPS = 1000000
+
+NAME="Brainfuck interpreter"
+DESC="Interprets brainfuck programs"
+
+def initModule(cod):
+    cod.botcommands["BF"] = [bf_wrapper]
+
+def destroyModule(cod):
+    del cod.botcommands["BF"]
+
+def rehash():
+    pass
+
+def bf_wrapper(cod, line, splitline, source, destination):
+    if len(splitline) < 2:
+        cod.reply(source, destination, "BF <prog> -- executes brainfuck program <prog>")
+        return
+
+    cod.reply(source, destination, "> " + bf(splitline[1]))
+
+def bf(inp):
+    ".bf <prog> -- executes brainfuck program <prog>"""
+
+    program = re.sub('[^][<>+-.,]', '', inp)
+
+    # create a dict of brackets pairs, for speed later on
+    brackets = {}
+    open_brackets = []
+    for pos in range(len(program)):
+        if program[pos] == '[':
+            open_brackets.append(pos)
+        elif program[pos] == ']':
+            if len(open_brackets) > 0:
+                brackets[pos] = open_brackets[-1]
+                brackets[open_brackets[-1]] = pos
+                open_brackets.pop()
+            else:
+                return 'unbalanced brackets'
+    if len(open_brackets) != 0:
+        return 'unbalanced brackets'
+
+    # now we can start interpreting
+    ip = 0        # instruction pointer
+    mp = 0        # memory pointer
+    steps = 0
+    memory = [0] * BUFFER_SIZE  # initial memory area
+    rightmost = 0
+    output = ""   # we'll save the output here
+
+    # the main program loop:
+    while ip < len(program):
+        c = program[ip]
+        if   c == '+':
+            memory[mp] = memory[mp] + 1 % 256
+        elif c == '-':
+            memory[mp] = memory[mp] - 1 % 256
+        elif c == '>':
+            mp += 1
+            if mp > rightmost:
+                rightmost = mp
+                if mp >= len(memory):
+                    # no restriction on memory growth!
+                    memory.extend([0] * BUFFER_SIZE)
+        elif c == '<':
+            mp = mp - 1 % len(memory)
+        elif c == '.':
+            output += chr(memory[mp])
+            if len(output) > 500:
+                break
+        elif c == ',':
+            memory[mp] = random.randint(1, 255)
+        elif c == '[':
+            if memory[mp] == 0:
+                ip = brackets[ip]
+        elif c == ']':
+            if memory[mp] != 0:
+                ip = brackets[ip]
+
+        ip += 1
+        steps += 1
+        if steps > MAX_STEPS:
+            if output == '':
+                output = '(no output)'
+            output += '[exceeded %d iterations]' % MAX_STEPS
+            break
+
+    stripped_output = re.sub(r'[\x00-\x1F]', '', output)
+
+    if stripped_output == '':
+        if output != '':
+            return 'no printable output'
+        return 'no output'
+
+    return stripped_output[:430].decode('utf8', 'ignore')
