8bd3f40ee13ea52fe08d082e297c99887daa5d71
Sam Dodrill <shadow.h511@gmail.com>
2013-11-07 18:38:21 -0500

    Add fibbonacci number calculator

---
+++ modules/bot/fibbonacci.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+NAME="fibbonacci number generator"
+DESC="Outputs fibbonacci numbers"
+
+def initModule(cod):
+    cod.botcommands["FIB"] = [fibCMD]
+
+def destroyModule(cod):
+    del cod.botcommands["FIB"]
+
+def fibCMD(cod, line, splitline, source, destination):
+    if len(splitline) < 2:
+        cod.reply(source, destination, "Syntax: FIB <num> - outputs num'th fibbonacci number")
+        return
+
+    num = int(splitline[1])
+
+    cod.reply(source, destination, "%d: %d" % (num, smart_recur_fib(num)))
+
+fibs = {}
+
+def smart_recur_fib(x):
+    global fibs
+
+    if x not in fibs:
+        if x <= 2:
+            fibs[x] = 1;
+        else:
+            fibs[x] = smart_recur_fib(x-1) + smart_recur_fib(x-2)
+    return fibs[x]
+
