ae964b917dfc7b2b020aa85ef27e03c80193b037
Sam Dodrill <shadow.h511@gmail.com>
2013-11-08 06:17:54 -0500

    Port skybot's memusage command over

---
+++ modules/bot/memusage.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+def initModule(cod):
+    cod.botcommands["MEM"] = [cmdMEM]
+
+def destroyModule(cod):
+    del cod.botcommands["MEM"]
+
+def rehash():
+    pass
+
+def cmdMEM(cod, line, splitline, source, destination):
+    cod.reply(source, destination, mem())
+
+def mem():
+    status_file = open("/proc/%d/status" % os.getpid()).read()
+    line_pairs = re.findall(r"^(\w+):\s*(.*)\s*$", status_file, re.M)
+    status = dict(line_pairs)
+    keys = 'VmSize VmLib VmData VmExe VmRSS VmStk'.split()
+
+    return ', '.join(key + ':' + status[key] for key in keys)
+
