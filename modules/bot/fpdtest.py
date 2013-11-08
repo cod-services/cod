1627eb9540f6be5e64bc1a0e356fa8d878009182
Sam Dodrill <shadow.h511@gmail.com>
2013-11-07 21:30:26 -0500

    Fix error in fpdtest

--- modules/bot/fpdtest.py
+++ modules/bot/fpdtest.py
@@ -36,7 +36,7 @@ def destroyModule(cod):
     del cod.botcommands["FPDTEST"]
 
 def fpdtestCMD(cod, line, splitline, source, destination):
-    if(failIfNotOper(cod, client, dest)):
+    if(failIfNotOper(cod, source, dest)):
         return
 
     if len(splitline) < 3:
