27c021f70f84593587ddab56e1894eeced1e098e
Sam Dodrill <shadow.h511@gmail.com>
2013-12-04 10:38:18 -0800

    services/killonfailoper: New module that kills on failed oper attempts
    Note that client was killed
    
    Show public kill reason

---
+++ modules/services/killonfailoper.py
@@ -0,0 +1,57 @@
+"""
+Copyright (c) 2013, Sam Dodrill
+All rights reserved.
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+claim that you wrote the original software. If you use this software
+in a product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+
+  2. Altered source versions must be plainly marked as such, and must not be
+misrepresented as being the original software.
+
+  3. This notice may not be removed or altered from any source
+distribution.
+"""
+
+# In an ideal world, this would not be needed.
+
+def initModule(cod):
+    cod.s2scommands["ENCAP"].append(killOnFailOper)
+
+def destroyModule(cod):
+    cod.s2scommands["ENCAP"].remove(killOnFailOper)
+
+def rehash():
+    pass
+
+def killOnFailOper(cod, line):
+    if not line.args[1] == "SNOTE":
+        return
+
+    if not line.args[2] == "s":
+        return
+
+    if not line.args[-1].split()[1] == "OPER":
+        return
+
+    offendingclient = cod.findClientByNick(line.args[-1].split()[7])
+
+    cod.notice(offendingclient.uid, "You may not attempt to become an operator without being on staff")
+    cod.notice(offendingclient.uid, "Staff has been notified.")
+
+    cod.servicesLog("%s FAILED OPER attempt: (%s@%s : %s), killed" % (offendingclient.nick,
+        offendingclient.user, offendingclient.host, offendingclient.ip))
+
+    cod.sendLine(":%s KILL %s :spacing (Failed OPER attempt)" % (cod.client.uid, offendingclient.uid))
+
+    cod.clients.pop(offendingclient.uid)
+
