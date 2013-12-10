ffc391a20d9917f441a034c2116935d82a740f3a
Sam Dodrill <shadow.h511@gmail.com>
2013-12-10 08:11:47 -0800

    New bot module youtube
    This does automatic lookups on Youtube links and searches on Youtube
    videos by arbitrary strings

---
+++ modules/bot/youtube.py
@@ -0,0 +1,78 @@
+"""
+Copyright (c) 2013, Sam Dodrill
+All rights reserved.
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+"""
+
+import requests
+import re
+
+NAME="Youtube lookups"
+DESC="Youtube searching and title lookups"
+
+def initModule(cod):
+    cod.s2scommands["PRIVMSG"].append(youtubeLookup)
+    cod.botcommands["YT"] = [youtubeSearch]
+
+def destroyModule(cod):
+    cod.s2scommands["PRIVMSG"].remove(youtubeLookup)
+    del cod.botcommands["YT"]
+
+def rehash():
+    pass
+
+def youtubeLookup(cod, line):
+    if line.args[0] not in cod.channels:
+        return
+
+    client = cod.clients[line.source]
+    regex = re.compile('(youtube.com/watch\S*v=|youtu.be/)([\w-]+)')
+    chatline = line.args[-1]
+
+    if "youtube" not in chatline:
+        return
+
+    videoid = regex.split(chatline)[2]
+
+    try:
+        info = requests.get("http://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=json" % videoid).json()
+
+        string = "^ Youtube: %s" % info["entry"]["title"]["$t"]
+
+        cod.privmsg(line.args[0], string)
+    except Exception as e:
+        cod.privmsg(line.args[0], "There was some error looking up that video: %s" % e.message)
+
+def youtubeSearch(cod, line, splitline, source, destination):
+    search = " ".join(splitline[1:])
+
+    try:
+        info = requests.get("https://gdata.youtube.com/feeds/api/videos?q=%s&v=2&alt=jsonc" % search).json()
+
+        video = info["data"]["items"][0]
+
+        string = "Youtube: %s - http://youtu.be/%s" % (video["title"], video["id"])
+
+        cod.reply(source, destination, string)
+
+    except Exception as e:
+        cod.reply(source, destination, "There was some error looking up that video: %s" % e.message)
+
