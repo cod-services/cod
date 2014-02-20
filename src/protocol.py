dfd1e7a291a57aa299f6556cbb0d0d6d8afff9a3
Sam Dodrill <shadow.h511@gmail.com>
2014-02-20 07:40:20 -0800

    src/protocol: remove crufty line

--- src/protocol.py
+++ src/protocol.py
@@ -112,7 +112,6 @@ class TS6ServerConn():
 
 class InspircdServerConn(TS6ServerConn):
     def __init__(self, cod):
-        TS6ServerConn.__init__(self, cod)
         self.umodes = "+iok"
 
     def join_client(self, client, channel):
