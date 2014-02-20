b420a9a4f74f8e93d6a64b2922f3d7dd66a5d67e
Sam Dodrill <shadow.h511@gmail.com>
2014-02-20 11:44:20 -0800

    Add first part of templating code

--- src/web.py
+++ src/web.py
@@ -23,6 +23,8 @@ freely, subject to the following restrictions:
 """
 
 import cherrypy
+from mako.lookup import TemplateLookup
+
 
 class RootPage:
     def __init__(self, cod):
@@ -41,4 +43,50 @@ class RootPage:
     @cherrypy.expose
     def index(self):
         return "Hello!"
+
+# http://tools.cherrypy.org/wiki/Mako
+
+class MakoHandler(cherrypy.dispatch.LateParamPageHandler):
+    """Callable which sets response.body."""
+
+    def __init__(self, template, next_handler):
+        self.template = template
+        self.next_handler = next_handler
+
+    def __call__(self):
+        env = globals().copy()
+        env.update(self.next_handler())
+        try:
+            return self.template.render(**env)
+        except:
+            # something went wrong rendering the template
+            # this will generate a pretty error page with details
+            cherrypy.response.status = "500"
+            return exceptions.html_error_template().render()
+
+class MakoLoader(object):
+
+    def __init__(self):
+        self.lookups = {}
+
+    def __call__(self, filename, directories, module_directory=None,
+                 collection_size=-1):
+        # Find the appropriate template lookup.
+        key = (tuple(directories), module_directory)
+        try:
+            lookup = self.lookups[key]
+        except KeyError:
+            lookup = TemplateLookup(directories=directories,
+                                    module_directory=module_directory,
+                                    collection_size=collection_size,
+                                    )
+            self.lookups[key] = lookup
+        cherrypy.request.lookup = lookup
+
+        # Replace the current handler.
+        cherrypy.request.template = t = lookup.get_template(filename)
+        cherrypy.request.handler = MakoHandler(t, cherrypy.request.handler)
+
+main = MakoLoader()
+cherrypy.tools.mako = cherrypy.Tool('on_start_resource', main)
 
