0343fd15aa86cbfb5f57348d1804c37b2f4c8d5f
Sam Dodrill <shadow.h511@gmail.com>
2014-02-24 20:03:43 -0800

    services/helprelay: Add new help channel to staff channel relay

---
+++ modules/services/helprelay.py
@@ -0,0 +1,17 @@
+"""
+Simple relay from #help tp #opers
+
+Code is in public domain
+"""
+
+def initModule(cod):
+    cod.addHook("chanmsg", chanmsgHook)
+
+def destroyModule(cod):
+    cod.delHook("chanmsg", chanmsgHook)
+
+def chanmsgHook(cod, target, line):
+    if target.name == cod.config["etc"]["helpchan"]:
+        cod.privmsg(cod.config["etc"]["staffchan"], "%s: <-%s> %s" %\
+                (target.name, line.source.nick, line.args[-1]))
+
