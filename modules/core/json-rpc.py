cabd5dcdeba69703a698b60dbb8fee4ecddcdb56
Sam Dodrill <shadow.h511@gmail.com>
2014-04-04 22:48:06 -0700

    core/json-rpc: Add JSON RPC frontend

---
+++ modules/core/json-rpc.py
@@ -0,0 +1,75 @@
+import cherrypy
+import json
+
+from structures import makeClient
+from web import RootPage
+
+def initModule(cod):
+    cod.webapp.root.add_page("json", JsonAPI)
+
+def destroyModule(cod):
+    cod.webapp.root.del_page("json")
+
+class JsonAPI(RootPage):
+    def __init__(self, cod):
+        self.cod = cod
+
+    @cherrypy.expose
+    def command(self, cmd, *args):
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+
+        cmd = str(cmd)
+        source = make_smurf()
+        destination = "<json-rpc>"
+        line = cmd.upper() + " " + " ".join(args)
+        splitline = args
+
+        print line, splitline
+
+        output = None
+
+        if cmd in self.cod.botcommands:
+            for impl in self.cod.botcommands[cmd]:
+                output = impl(self.cod, line, splitline, source, destination)
+                return format_reply(output)
+        else:
+            return format_reply("No such command", True)
+
+        print output
+
+    @cherrypy.expose
+    def clientlist(self):
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+
+        clients = []
+
+        for client in self.cod.clients.itervalues():
+            clients.append(json.loads(client.json()))
+
+        return format_reply(clients)
+
+    @cherrypy.expose
+    def channellist(self):
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+
+        channels = []
+
+        for channel in self.cod.channels.itervalues():
+            channels.append(json.loads(channel.json()))
+
+        return format_reply(channels)
+
+def make_smurf():
+    return makeClient("<json-rpc>", "*false*", "Fake!uSer@LOLOL", "Json-RPC", "000000000")
+
+def format_reply(output, error=False):
+    toSend = {}
+    if error:
+        toSend["status"] = "error"
+    else:
+        toSend["status"] = "okay"
+
+    toSend["output"] = output
+
+    return json.dumps(toSend)
+
