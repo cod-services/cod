d722f9af5520b135ac0454d88f634174b45e045b
Sam Dodrill <xena@yolo-swag.com>
2014-06-10 04:40:01 +0000

    src/utils: add utility dict class

--- src/utils.py
+++ src/utils.py
@@ -22,7 +22,12 @@ freely, subject to the following restrictions:
     distribution.
 """
 
-from structures import *
+class CaseInsensitiveDict(dict):
+    def __setitem__(self, key, value):
+        super(CaseInsensitiveDict, self).__setitem__(key.lower(), value)
+
+    def __getitem__(self, key):
+        return super(CaseInsensitiveDict, self).__getitem__(key.lower())
 
 def failIfNotOper(cod, client, dest):
     """
