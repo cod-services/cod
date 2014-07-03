5908942d194f86372363e8c86c351765567e108e
Sam Dodrill <xena@yolo-swag.com>
2014-07-03 15:37:43 -0700

    Add some channel handling functions

--- bot/channel.go
+++ bot/channel.go
@@ -1,5 +1,10 @@
 package cod
 
+import (
+	"errors"
+	"strings"
+)
+
 type ChanUser struct {
 	Client  *Client
 	Channel *Channel
@@ -14,12 +19,37 @@ type Channel struct {
 	Lists   map[string][]string
 }
 
-func NewChannel(name string, ts int64) (c *Channel) {
+func (cod *Cod) NewChannel(name string, ts int64) (c *Channel) {
 	c = &Channel{
-		Name:  name,
-		Ts:    ts,
-		Lists: make(map[string][]string),
+		Name:    name,
+		Ts:      ts,
+		Lists:   make(map[string][]string),
+		Clients: make(map[string]*ChanUser),
+		Modes:   0,
 	}
+
+	cod.Channels[strings.ToUpper(name)] = c
 
 	return
 }
+
+func (c *Channel) AddChanUser(client Client) (cu *ChanUser) {
+	cu.Client = &client
+	cu.Channel = c
+	cu.Prefix = 0
+
+	c.Clients[client.Uid()] = cu
+
+	return
+}
+
+func (c *Channel) DelChanUser(client Client) (err error) {
+	if _, ok := c.Clients[client.Uid()]; !ok {
+		return errors.New("Tried to delete nonexistent chanuser with uid "+client.Uid()+" from "+c.Name)
+	}
+
+	delete(c.Clients, client.Uid())
+
+	return nil
+}
+
