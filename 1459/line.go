7c751df7cade29517d6b0b9baba4e39b6f9a2b26
Sam Dodrill <xena@yolo-swag.com>
2014-07-02 22:54:58 -0700

    Add 1459 library for IRC line parsing

---
+++ 1459/line.go
@@ -0,0 +1,41 @@
+package 1459
+
+import "strings"
+
+// IRC line
+type RawLine struct {
+	Source string
+	Verb   string
+	Args   []string
+}
+
+// Create a new line and split out an RFC 1459 frame to a Line
+func NewLine(input string) (line *Line) {
+	line = &Line{}
+
+	split := strings.Split(input, " ")
+
+	if split[0][0] == ':' {
+		line.Source = split[0][1:]
+		line.Verb = split[1]
+		split = split[2:]
+	} else {
+		line.Source = ""
+		line.Verb = split[0]
+		split = split[1:]
+	}
+
+	argstring := strings.Join(split, " ")
+	extparam := strings.Split(argstring, " :")
+
+	if len(extparam) > 1 {
+		ext := strings.Join(extparam[1:], " :")
+		args := strings.Split(extparam[0], " ")
+
+		line.Args = append(args, ext)
+	} else {
+		line.Args = split
+	}
+
+	return
+}
